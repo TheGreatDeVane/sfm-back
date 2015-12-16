1fcddd1832fdfdcabd2ed66b255c016660c90ae9
Andy DeVane <ad.mgld50@gmail.com>
2015-12-16 15:04:08 -0500

    change paperclip model url

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,8 +2,8 @@ class Picture < ActiveRecord::Base
   belongs_to :gallery
 
   has_attached_file :image,
-    :path => ":rails_root/public/images/:id/:filename",
-    :url  => "/images/:id/:filename"
+    :path => "/images/:id/:style.:extension",
+                  :url => ":s3_domain_url"
 
   do_not_validate_attachment_file_type :image
 end
